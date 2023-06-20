aa9fd6c7744f2adf5959e9c2d4b5e27ebb8981d6
BuildTools <unconfigured@null.spigotmc.org>
2023-06-20 15:41:35 -0400

    Deleted not used functions

--- Generate_reference.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def reference(step_start, init_value, final_value, run_time, Ts):
-    ref = np.ones(1,step_start)
--- MFC_linear_controller.py
+++
@@ -1,5 +0,0 @@
-# Linear Feedback Controller of a PD MFC Structure
-
-def linearController(e, e_dot, K1, K2, K3):
-    u = K2*e_dot + K1*e
-    return u
